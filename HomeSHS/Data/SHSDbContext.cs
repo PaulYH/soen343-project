f7f6deab8615ce4c00410a9eb2f0eeccf0790e03
PaulYH <paulhumenny@icloud.com>
2024-03-09 00:26:07 -0500

    Add database for users

---
+++ HomeSHS/Data/SHSDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SHC.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeSHS.Database
+{
+    public class SHSDbContext : DbContext
+    {
+        public SHSDbContext(DbContextOptions<SHSDbContext> options) : base(options) { }
+
+        public DbSet<VirtualUser> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
