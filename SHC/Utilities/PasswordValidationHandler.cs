ab1d41277c876b8ef295a04943587f0f6c443a9b
etiennepaquet1 <107227178+etiennepaquet1@users.noreply.github.com>
2024-03-12 23:21:37 -0400

    fixed password validation condition

--- SHC/Utilities/PasswordValidationHandler.cs
+++ SHC/Utilities/PasswordValidationHandler.cs
@@ -24,9 +24,9 @@ namespace SHC.Utilities
             string exceptionToThrow = "";
             if (request.Password != request.PasswordConfirm) { exceptionToThrow += "Passwords do not match.\n"; }
             if (request.Password.Length < 8) { exceptionToThrow += "Password is below the miminum length of 8 characters.\n"; }
-            if (request.Password.Any(char.IsDigit)) { exceptionToThrow += "Password does not contain a digit.\n"; }
-            if (request.Password.Any(char.IsLower)) { exceptionToThrow += "Password does not contain a lowercase letter.\n"; }
-            if (request.Password.Any(char.IsUpper)) { exceptionToThrow += "Password does not contain an uppercase letter."; }
+            if (!request.Password.Any(char.IsDigit)) { exceptionToThrow += "Password does not contain a digit.\n"; }
+            if (!request.Password.Any(char.IsLower)) { exceptionToThrow += "Password does not contain a lowercase letter.\n"; }
+            if (!request.Password.Any(char.IsUpper)) { exceptionToThrow += "Password does not contain an uppercase letter."; }
 
             if (exceptionToThrow != "") 
             {
