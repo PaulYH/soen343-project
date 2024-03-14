f449a736d180b2de0d78cb72a45a951927b7849f
PaulYH <paulhumenny@icloud.com>
2024-03-14 17:27:57 -0400

    Begin work on SHSTab

--- HomeSHS/Components/Helpers/IPageRefresher.cs
+++ HomeSHS/Components/Helpers/IPageRefresher.cs
@@ -3,9 +3,11 @@
     public interface IPageRefresher
     {
         event Action IndexRefreshRequested;
+        event Action SHSTabRefreshRequested;
         event Action SHCTabRefreshRequested;
         event Action SimulatorSettingsRefreshRequested;
         void CallIndexRequestRefresh();
+        void CallSHSTabRefreshRequested();
         void CallSHCTabRefreshRequested();
         void CallSimulatorSettingsRefresh();
     }
--- HomeSHS/Components/Helpers/PageRefresher.cs
+++ HomeSHS/Components/Helpers/PageRefresher.cs
@@ -3,12 +3,17 @@
     public class PageRefresher : IPageRefresher
     {
         public event Action IndexRefreshRequested;
+        public event Action SHSTabRefreshRequested;
         public event Action SHCTabRefreshRequested;
         public event Action SimulatorSettingsRefreshRequested;
 
         public void CallIndexRequestRefresh()
         {
             IndexRefreshRequested?.Invoke();
+        }
+        public void CallSHSTabRefreshRequested()
+        {
+            SHSTabRefreshRequested?.Invoke();
         }
 
         public void CallSHCTabRefreshRequested()
