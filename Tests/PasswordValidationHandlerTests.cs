bdbda98c7b9c18a286ff30786375363973118d35
etiennepaquet1 <107227178+etiennepaquet1@users.noreply.github.com>
2024-03-21 01:19:32 -0400

    added handler tests

---
+++ Tests/PasswordValidationHandlerTests.cs
@@ -0,0 +1,49 @@
+﻿using SHC.Controllers;
+using SHC.Services;
+using SHC.Utilities;
+using SHC.Entities;
+
+namespace Tests
+{
+    public class PasswordValidationHandlerTests
+    {
+        private readonly IFixture _fixture;
+        private readonly Mock<IUserService> _userServiceMock;
+        private readonly PasswordValidationHandler _handler;
+
+        public PasswordValidationHandlerTests()
+        {
+            _fixture = new Fixture();
+            _fixture.Behaviors.OfType<ThrowingRecursionBehavior>().ToList()
+                .ForEach(b => _fixture.Behaviors.Remove(b));
+            _fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+
+            _userServiceMock = _fixture.Freeze<Mock<IUserService>>();
+            _handler = new PasswordValidationHandler(_userServiceMock.Object);
+        }
+
+        [Fact]
+        public async void HandleRegisterRequest_ShouldReturnString_WhenBadPassword()
+        {
+            // Arrange
+            var request = _fixture.Create<RegisterRequest>();
+            request.Password = "";
+            request.PasswordConfirm = request.Password + " ";
+
+            // Act
+            var result = await _handler.HandleRegisterRequest(request);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().BeAssignableTo<string>();
+            result.Should().Be("Invalid Password: Passwords do not match.\n" +
+                "Password is below the miminum length of 8 characters.\n" +
+                "Password does not contain a digit.\n" +
+                "Password does not contain a lowercase letter.\n" +
+                "Password does not contain an uppercase letter.");
+        }
+
+
+
+    }
+}
---
+++ Tests/PermissionsHandlerTests.cs
@@ -0,0 +1,45 @@
+﻿using SHC.Controllers;
+using SHC.Services;
+using SHC.Utilities;
+using SHC.Entities;
+
+namespace Tests
+{
+    public class PermissionsHandlerTests
+    {
+        private readonly IFixture _fixture;
+        private readonly Mock<IUserService> _userServiceMock;
+        private readonly PermissionsHandler _handler;
+
+        public PermissionsHandlerTests()
+        {
+            _fixture = new Fixture();
+            _fixture.Behaviors.OfType<ThrowingRecursionBehavior>().ToList()
+                .ForEach(b => _fixture.Behaviors.Remove(b));
+            _fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+
+            _userServiceMock = _fixture.Freeze<Mock<IUserService>>();
+            _handler = new PermissionsHandler(_userServiceMock.Object);
+        }
+
+        [Fact]
+        public async void HandleRegisterRequest_ShouldReturnString_WhenValidRequest()
+        {
+            // Arrange
+            var request = _fixture.Create<RegisterRequest>();
+            _userServiceMock.Setup(x => x.HandleRegisterRequest(request)).ReturnsAsync("User Creation Successful! Please log in to continue.");
+
+
+            // Act
+            var result = await _handler.HandleRegisterRequest(request);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().BeAssignableTo<string>();
+            result.Should().Be("User Creation Successful! Please log in to continue.");
+        }
+
+
+
+    }
+}
