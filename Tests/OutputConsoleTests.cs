653c9da0a33d7c1d774d405d67db5c6de5b17703
iluvpesto <102493688+NourHadieHassoun@users.noreply.github.com>
2024-04-09 22:12:33 -0400

    Added Log_ShouldReturnVoid_WhenValidInput() to OutputConsoleTests.cs

--- Tests/OutputConsoleTests.cs
+++ Tests/OutputConsoleTests.cs
@@ -23,24 +23,7 @@ namespace Tests
             console = OutputConsole.GetInstance();
         }
 
-        [Fact]
-        public async void Log_ShouldReturnVoid_WhenValidInput()
-        {
-            string deviceId = _fixture.Create<string>();
-            string eventType = _fixture.Create<string>();
-            string eventDesc = _fixture.Create<string>();
-            string details = _fixture.Create<string>();
-            console.Log(deviceId, eventType, eventDesc, details);
-
-            var _outputLog = console.outputLog;
-            _outputLog.Should().NotBeNull().And.HaveCount(1);
-
-            var tuple = _outputLog.First();
-            tuple.deviceId.Should().Be(deviceId);
-            tuple.eventType.Should().Be(eventType);
-            tuple.eventDesc.Should().Be(eventDesc);
-            tuple.details.Should().Be(details);
-        }
+       
     }
 
 }
