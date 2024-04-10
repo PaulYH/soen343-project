f4ad70e8feb2584eca787b5a640ad4d4f0cdcea9
hisham-kitaz <111257875+hisham-kitaz@users.noreply.github.com>
2024-04-09 21:37:02 -0400

    Adding SmartHomeSecurityTests constructor

---
+++ Tests/SmartHomeSecurityTests.cs
@@ -0,0 +1,28 @@
+﻿using SHC.Utilities.State;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    public class SmartHomeSecurityTests
+    {
+        private readonly IFixture fixture;
+        private readonly Mock<IState> state;
+        private readonly SmartHomeSecurity shh;
+
+        public SmartHomeSecurityTests() 
+        {
+            fixture = new Fixture();
+            fixture.Behaviors.OfType<ThrowingRecursionBehavior>().ToList()
+                .ForEach(b => fixture.Behaviors.Remove(b));
+            fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+
+            shh = new SmartHomeSecurity();
+        }
+
+
+    }
+}
