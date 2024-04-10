bf064e60f74de4aeaa9293820044ccfabfd92ad0
iluvpesto <102493688+NourHadieHassoun@users.noreply.github.com>
2024-04-09 22:00:18 -0400

    Create SHHListenerTests.cs

---
+++ Tests/SHHListenerTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using SHC.Entities.Room;
+using SHC.Utilities.Observer;
+using SHC.Entities.Door;
+using SHC.Entities;
+using AutoFixture.Kernel;
+using AutoFixture;
+using SHC.Entities.Window;
+using SHC.Entities.Light;
+
+
+namespace Tests
+{
+    public class SHHListenerTests
+    {
+        private readonly IFixture _fixture;
+        private readonly SHHListener _listener;
+
+
+        public SHHListenerTests() 
+        {
+            _fixture = new Fixture();
+            _fixture.Behaviors.OfType<ThrowingRecursionBehavior>().ToList()
+                .ForEach(b => _fixture.Behaviors.Remove(b));
+            _fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+            _fixture.Customizations.Add(
+            new TypeRelay(
+                typeof(IRoom),
+                typeof(Entrance)));
+            _fixture.Customizations.Add(
+            new TypeRelay(
+                typeof(IDoor),
+                typeof(RegularDoor)));
+            _fixture.Customizations.Add(
+            new TypeRelay(
+                typeof(IWindow),
+                typeof(RegularWindow)));
+            _fixture.Customizations.Add(
+                new TypeRelay(
+                typeof(ILight),
+                typeof(RegularLight)));
+
+
+
+            List<IRoom> rooms = new List<IRoom>();
+            //var room1 = new Entrance();
+            //rooms.Add(room1);
+            var state = _fixture.Create <List<(int zoneNum, List<IRoom> rooms, double temp1, double temp2, double temp3)>>();
+            _listener = new SHHListener(state);
+        }
+
+
+        
+    }
+}
